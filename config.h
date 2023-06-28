745ece771b70de5553605edffb9237ab8fd9c732
ritorobbob <ritorobbob@gmail.com>
2023-06-28 02:18:10 +0100

    cci onfig changed to fit terms together bettevr

--- config.h
+++ config.h
@@ -1,6 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 /* appearance */
-#include <cwchar>
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
